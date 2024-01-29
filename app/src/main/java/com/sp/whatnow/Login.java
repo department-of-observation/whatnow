1a49cb2523cd4462ffce9b93d19d1c909ad710ec
Diamond4hire <kelvinzhong.22@ichat.sp.edu.sg>
2024-01-30 06:38:16 +0800

    Added Fragments and more
    Added Fragment for documents and Settings, Did the layout for both.
    
    modified theme.xml folder (Edit colors to linking)

--- app/src/main/java/com/sp/whatnow/Login.java
+++ app/src/main/java/com/sp/whatnow/Login.java
@@ -1,14 +1,16 @@
 package com.sp.whatnow;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.ActionBar;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_login);
     }
 }
