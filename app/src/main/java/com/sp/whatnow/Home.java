f0ce2656539ebebcec066222abcbeacc597ffbbc
Diamond4hire <kelvinzhong.22@ichat.sp.edu.sg>
2024-01-31 14:15:07 +0800

    more half complete nav drawer

--- app/src/main/java/com/sp/whatnow/Home.java
+++ app/src/main/java/com/sp/whatnow/Home.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -15,31 +16,52 @@ import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView navigationView;
     private DocumentsFragment documentsFragment;
     private SettingsFragment settingsFragment;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        documentsFragment = new DocumentsFragment();
-        settingsFragment = new SettingsFragment();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        // Set up ActionBarDrawerToggle
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.nav_open,
+                R.string.nav_close
+        );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // Handle item clicks in the NavigationView
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Handle item clicks here
+                int id = item.getItemId();
+                if (id == R.id.nav_documents) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.homeFragmentContainer, documentsFragment).commit();
+                } else if (id == R.id.nav_settings){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.homeFragmentContainer, settingsFragment).commit();
+                }
+
+
+                // Close the drawer after handling the item click
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-/*
-    NavigationView.OnNavigationItemSelectedListener navSelected = new NavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            int id = item.getItemId();
-            if (id == R.id.nav_documents){
-                fragment = new DocumentsFragment();
-            }
-            fragmentManager.beginTransaction().replace(R.id.homeFragmentContainer, fragment).commit();
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-    } */
 }
